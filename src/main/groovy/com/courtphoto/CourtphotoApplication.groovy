bb66a7f763b5101cfb461e3a3019d1a0ed929eb8
tws <xldzm3433>
2018-02-21 15:31:07 +0900

    firstCommit

---
+++ src/main/groovy/com/courtphoto/CourtphotoApplication.groovy
@@ -0,0 +1,29 @@
+package com.courtphoto
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.view.InternalResourceViewResolver
+
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+class CourtphotoApplication {
+
+
+	static void main(String[] args) {
+		SpringApplication.run CourtphotoApplication, args
+	}
+
+	@Bean
+	public InternalResourceViewResolver setupViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix('/templates/');
+		resolver.setSuffix('.jsp');
+		return resolver;
+	}
+
+}
---
+++ src/main/groovy/com/courtphoto/cont/TestController.groovy
@@ -0,0 +1,47 @@
+package com.courtphoto.cont
+
+import com.courtphoto.domain.Member
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+import javax.persistence.EntityManager
+import javax.persistence.EntityManagerFactory
+import javax.persistence.EntityTransaction
+
+/**
+ * Created by jihoonOh on 2018. 2. 20..
+ */
+@Controller
+class TestController {
+
+    @Autowired
+    EntityManagerFactory entityManagerFactory;
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public Object saveConfig() {
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        Member m = new Member();
+        m.setAge(10);
+        m.setId('ojh9134');
+        m.setUserName('jihoonOh');
+        em.persist(m);
+        tx.commit();
+
+        em.close();
+
+        return 'test';
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
